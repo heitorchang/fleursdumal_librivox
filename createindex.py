1c41fbea0d8a8840dd4034617067f5ced622793c
Heitor Chang <heitorchang@gmail.com>
2021-01-31 14:15:50 -0300

    poem 40

--- createindex.py
+++ createindex.py
@@ -14,19 +14,42 @@ def createindex():
             <pre>
     LES FLEURS DU MAL
 
-      de
+      by
         
     CHARLES BAUDELAIRE
+
+
+Poem numbers follow <a href="https://librivox.org/les-fleurs-du-mal-by-charles-baudelaire/">LibriVox's order</a>.        
     """, file=idx)
 
         print("Missing poems: ")
         with open("librivoxlist.txt", encoding="utf-8") as lst:
             for i, line in enumerate(lst):
                 if os.path.exists(str(i) + ".txt"):
-                    print(f"""<a href="{i}.html">{line}</a>""", file=idx)
+                    tokens = line.split()
+                    num = int(tokens[0])
+                    disp = str(num) + " " + " ".join(tokens[1:]) + "\n"
+                    print(f"""<a href="{i}.html">{disp}</a>""", file=idx)
                 else:
                     print(str(i), end=" ")
 
+        print("""
+ Credits:
+        
+ Text:
+
+        <a href="http://www.gutenberg.org/ebooks/6099">Project Gutenberg</a>
+        
+        <a href="https://fleursdumal.org/alphabetical-listing">fleursdumal.org</a>
+        
+ Audio:
+
+        <a href="https://librivox.org/les-fleurs-du-mal-by-charles-baudelaire/">LibriVox</a>
+
+ Conversion scripts:
+
+        <a href="https://github.com/heitorchang/fleursdumal_librivox">fleursdumal_librivox on GitHub</a>
+        """, file=idx)
         print("</pre></body></html>", file=idx)
     print()
     print("End of createindex.")
